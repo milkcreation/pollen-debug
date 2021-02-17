c404f7e2f768a7c3325e79be10bc0c5138ceb902
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-17 08:50:02 +0000

    debug >> 1.0.x-dev

---
+++ src/DebugBarInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Debug;
+
+interface DebugBarInterface
+{
+    /**
+     * Récupération du pied de page du site
+     *
+     * @return string
+     */
+    public function renderFooter(): string;
+
+    /**
+     * Récupération de l'entête du site
+     *
+     * @return string
+     */
+    public function renderHead(): string;
+
+    /**
+     * Récupération du rendu de l'affichage
+     *
+     * @return string
+     */
+    public function render(): string;
+}
