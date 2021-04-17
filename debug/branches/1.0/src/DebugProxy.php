5831bd272cfab133ca4991693b1591ecd30925e1
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-04-17 11:57:58 +0000

    debug >> 1.0.x-dev

---
+++ debug/branches/1.0/src/DebugProxy.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Debug;
+
+use Pollen\Support\StaticProxy;
+use RuntimeException;
+
+/**
+ * @see \Pollen\Debug\DebugProxyInterface
+ */
+trait DebugProxy
+{
+    /**
+     * Instance du gestionnaire de débogage.
+     * @var DebugManagerInterface
+     */
+    private $debugManager;
+
+    /**
+     * Instance du gestionnaire de débogage.
+     *
+     * @return DebugManagerInterface
+     */
+    public function debug(): DebugManagerInterface
+    {
+        if ($this->debugManager === null) {
+            try {
+                $this->debugManager = DebugManager::getInstance();
+            } catch (RuntimeException $e) {
+                $this->debugManager = StaticProxy::getProxyInstance(
+                    DebugManagerInterface::class,
+                    DebugManager::class,
+                    method_exists($this, 'getContainer') ? $this->getContainer() : null
+                );
+            }
+        }
+
+        return $this->debugManager;
+    }
+
+    /**
+     * Définition du gestionnaire de débogage.
+     *
+     * @param DebugManagerInterface $debugManager
+     *
+     * @return void
+     */
+    public function setDebugManager(DebugManagerInterface $debugManager): void
+    {
+        $this->debugManager = $debugManager;
+    }
+}
